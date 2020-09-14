18108d73e386a7b8c47e9b33db37f876978a0a9f
rjwthree <rjwthree@gmail.com>
2020-09-14 13:32:53 -0700

    Update Martingale.py

--- Martingale.py
+++ Martingale.py
@@ -1,17 +1,15 @@
 import random as rm
 import datetime as dt
-import numpy as np
 
 s = b = 10 # starting bet, current bet
+p = 48 # probability of winning (%)
 c = 0 # 1 if ceiling exists, 0 if not
 f = 5 # number of losing bets required to reach the ceiling
 n = 10**7 # number of hands
 u = 10**6 # progress updates will print at multiples of this number
-p = 48 # probability of winning (%)
 w = count = 0 # net winnings, counter
-v = 2**f*s # value of the ceiling
-N = [] # all net winnings will be appended here
-Q = [b] # all current bets will be appended here
+v = s*2**f # value of the ceiling
+N = Q = [] # all net winnings, current bets will be appended here
 
 print(f'Generating {n} random integers...')
 hands = [rm.randint(1,100) for i in range(n)] # generate n random integers between 1 and 100
@@ -24,16 +22,18 @@ for i in hands: # for each hand
             b = s # return current bet to starting bet
     else: # if win occurs
         w += b # add current bet to net winnings
-        b = s # always return current bet to starting bet after winning
+        b = s # always return current bet to starting bet
+        
     N.append(w) # append net winnings after each hand
     Q.append(b) # append current bet after each hand
+    
     count += 1 # increment counter
     if count == 1: # print start with timestamp
         print('Start at ' + dt.datetime.now().strftime('%H:%M:%S %Y-%m-%d'))
     if count % u == 0: # print updates with timestamps
         print(str(count) + ' at ' + dt.datetime.now().strftime('%H:%M:%S %Y-%m-%d'))
     
-Q.pop() # remove last bet because it was not placed
+Q = [s] + Q[:-1] # add starting bet, remove last bet because it was not placed
 
 print(str(w/n) + ' per hand, on average')
 print(str(min(N)) + ' at the nadir')
